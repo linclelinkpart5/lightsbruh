c6aaac0e59fd824aeae2a5b16818d5fae12b7d06
Justin J. Meza <justin.meza@gmail.com>
2023-01-05 18:31:26 -0800

    Added cpal and ALSA build environment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+
+    // Test some USB audio stuff!
+    println!("Testing out audio capabilities...");
+    use cpal::traits::{HostTrait, DeviceTrait};
+    let host = cpal::default_host();
+    let device_iter = host.devices().expect("Cannot retrieve host devices!");
+    println!("Found {} audio devices.", host.devices().expect("Cannot retrieve host devices!").count());
+    for device in device_iter {
+        println!("Device: {}.", device.name().expect("Cannot retrieve device name!"));
+    }
 }
